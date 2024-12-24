03030fad92b29c966ad8da0087cb13c8e63846f1
jadegong <jadegong1994@gmail.com>
2024-12-24 17:28:29 +0800

    <feat>(algorithm): Add dp algorithm jump game.

--- src/algorithm/dp_algorithm.rs
+++ src/algorithm/dp_algorithm.rs
@@ -27,4 +27,29 @@ pub fn max_profit_121(prices: Vec<i32>) -> i32 {
     return max_ret;
 }
 
+///
+/// Leetcode 55 jump game
+///You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+/// Return true if you can reach the last index, or false otherwise.
+///
+pub fn can_jump_55(nums: Vec<i32>) -> bool {
+    let mut index: usize = 0;
+    let mut max_arrive: usize = 0;
+    let nums_len = nums.len();
+    let mut current_num: usize;
+    while index < nums_len - 1 {
+        current_num = nums[index] as usize;
+        if current_num == 0 {
+            if max_arrive <= index {
+                return false;
+            }
+        }
+        if index + current_num > max_arrive {
+            max_arrive = index + current_num;
+        }
+        index += 1;
+    }
+    return true;
+}
+
 
