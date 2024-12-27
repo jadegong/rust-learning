f052d8727aca9232593492247906674b5d1b2d92
jadegong <jadegong1994@gmail.com>
2024-12-27 11:36:46 +0800

    <feat>(algorithm): Longest common prefix.

--- src/algorithm/string_algorithms.rs
+++ src/algorithm/string_algorithms.rs
@@ -18,3 +18,46 @@ pub fn length_of_last_word_58(s: String) -> i32 {
     }
     return ret_len;
 }
+
+///
+/// Leetcode 14
+///
+pub fn longest_common_prefix_14(strs: Vec<String>) -> String {
+    let strs_len = strs.len();
+    let mut min_len = strs[0].len();
+    let mut index = 1;
+    while index < strs_len {
+        if strs[index].len() <  min_len {
+            min_len = strs[index].len();
+        }
+        index += 1;
+    }
+    index = 0;
+    let mut ret_str = String::new();
+    'min_len_loop: loop {
+        if index >= min_len {
+            break 'min_len_loop;
+        }
+        let mut is_same = true;
+        let mut strs_len_index = 1;
+        let current_char = strs[0].get(index..(index+1)).unwrap();
+        // loop to judge if char at index is same
+        'strs_len_loop: loop {
+            if strs_len_index >= strs_len {
+                break 'strs_len_loop;
+            }
+            if current_char != strs[strs_len_index].get(index..(index+1)).unwrap() {
+                is_same = false;
+                break 'strs_len_loop;
+            }
+            strs_len_index += 1;
+        }
+        if is_same {
+            ret_str.push_str(current_char);
+            index += 1;
+        } else {
+            break 'min_len_loop;
+        }
+    }
+    return ret_str;
+}
--- src/tests/tests_algorithm/tests_string_algorithms.rs
+++ src/tests/tests_algorithm/tests_string_algorithms.rs
@@ -12,3 +12,13 @@ pub fn test_length_of_last_word_58() {
     let test_ret = string_algorithms::length_of_last_word_58(test_s);
     assert_eq!(test_ret, 6);
 }
+
+#[test]
+pub fn test_longest_common_prefix_14() {
+    let test_strs: Vec<String> = vec![String::from("flower"), String::from("flow"), String::from("flight")];
+    let expected_ret = string_algorithms::longest_common_prefix_14(test_strs);
+    assert_eq!(expected_ret, String::from("fl"));
+    let test_strs: Vec<String> = vec![String::from("dog"), String::from("racecar"), String::from("car")];
+    let expected_ret = string_algorithms::longest_common_prefix_14(test_strs);
+    assert_eq!(expected_ret, String::from(""));
+}
